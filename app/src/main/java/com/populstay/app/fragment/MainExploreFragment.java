c46bfba82b3a4c9e8fb48442473a6bebc2cd2591
Jerry Lau <jerry521610@163.com>
2018-07-18 15:58:01 +0800

    Update User Interface at Homepage

--- app/src/main/java/com/populstay/app/fragment/MainExploreFragment.java
+++ app/src/main/java/com/populstay/app/fragment/MainExploreFragment.java
@@ -1,20 +1,14 @@
 package com.populstay.app.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.populstay.app.R;
 import com.populstay.app.base.BaseFragment;
-
-import org.xutils.common.Callback;
-import org.xutils.http.RequestParams;
-import org.xutils.x;
 
 /**
  * 底部导航栏“探索” Fragment
@@ -28,8 +22,6 @@ public class MainExploreFragment extends BaseFragment implements View.OnClickLis
 	private EditText mEtAdults;
 	private EditText mEtChildren;
 	private Button mBtnSearch;
-	private TextView mTvResult;
-	private TextView mTvUrl;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,8 +38,6 @@ public class MainExploreFragment extends BaseFragment implements View.OnClickLis
 		mEtAdults = view.findViewById(R.id.et_adults);
 		mEtChildren = view.findViewById(R.id.et_children);
 		mBtnSearch = view.findViewById(R.id.btn_search_home);
-		mTvResult = view.findViewById(R.id.tv_result);
-		mTvUrl = view.findViewById(R.id.tv_url);
 	}
 
 	private void setListener() {
@@ -56,36 +46,6 @@ public class MainExploreFragment extends BaseFragment implements View.OnClickLis
 
 	@Override
 	public void onClick(View view) {
-
-		String url = "https://server.populstay.com/HouseInformation?place=Tokyo&guests=1&to=1539550000000&from=1529550000000&districeCode=0x3333322d30303332000000000000000000000000000000000000000000000000";
-		mTvUrl.setText(url);
-		RequestParams params = new RequestParams(url);
-		x.http().get(params, new Callback.CacheCallback<String>() {
-			@Override
-			public boolean onCache(String result) {
-				return false;
-			}
-
-			@Override
-			public void onSuccess(String result) {
-//				mTvResult.setText(result);
-				Log.d("result: ",  result);
-			}
-
-			@Override
-			public void onError(Throwable ex, boolean isOnCallback) {
-				Log.d("FAIL", ex + " " + isOnCallback);
-			}
-
-			@Override
-			public void onCancelled(CancelledException cex) {
-
-			}
-
-			@Override
-			public void onFinished() {
-
-			}
-		});
+		myToast("Search Home");
 	}
 }
